7c4f3b4d1f8501ec2f8ad4dc54e784b36578cff3
jcvdav <juancarlos.villader@gmail.com>
2019-07-30 14:47:04 -0700

    Added downloading script to get mex purs eseine raster from GBQ

---
+++ scripts/1_processing/2_download_mexican_purse_seiners_monthly_raster_by_vessel.R
@@ -0,0 +1,21 @@
+##############################################################
+## download_mexican_purse_seiners_monthly_raster_by_vessel ##
+##############################################################
+
+# Downloads the table from GBQ, performs a filter,
+# and then output it to the data folder
+
+# Load packages
+library(startR)
+library(here)
+
+# Get the table from GBQ
+mex_ps <- get_table(project = "ucsb-gfw",
+                    dataset = "jc_predictable_fish",
+                    table = "mexican_purse_seiners_monthly_raster_by_vessel") %>%
+  filter(between(lat_bin_center, -30, 35),
+         between(lon_bin_center, -160, 0))
+
+# Export the table as an RDS object
+saveRDS(object = mex_ps,
+        file = here("data", "mexican_purse_seiners_monthly_raster_by_vessel.rds"))
